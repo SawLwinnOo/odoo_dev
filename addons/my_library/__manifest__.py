4e49da1656877d4b7a920a83d2115cfb471d16c5
SawLwinnOo <sawlwinnoo01@gmail.com>
2023-10-12 16:27:53 +0630

    my library

---
+++ addons/my_library/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Library",
+
+    'author': "Saw Lwinn Oo",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/group.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
