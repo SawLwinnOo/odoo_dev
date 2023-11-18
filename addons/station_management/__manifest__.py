2f334bd943629b17e2d52e1085cb6ddc7d286ce5
SawLwinnOo <sawlwinnoo01@gmail.com>
2023-11-18 23:39:10 +0630

    action/button

--- addons/station_management/__manifest__.py
+++ addons/station_management/__manifest__.py
@@ -10,7 +10,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','fleet'],
+    'depends': ['base','fleet','account'],
 
     # always loaded
     'data': [
--- addons/station_management/models/booking.py
+++ addons/station_management/models/booking.py
@@ -7,11 +7,11 @@ class StationBooking(models.Model):
     _description = 'Station_Booking'
 
     name = fields.Char(default=lambda self: _("New"))
-    customer = fields.Many2one('res.partner',related='car_id.driver_id', string="Customer")
-    car_id = fields.Many2one("fleet.vehicle", string="Car", tracking=True,)
-    station_id = fields.Many2one("station.management", string="Station")
+    customer = fields.Many2one('res.partner', related='car_id.driver_id', string="Customer")
+    car_id = fields.Many2one("fleet.vehicle", string="Car", tracking=True, )
+    station_id = fields.Many2one("station.management", string="Station", tracking=True)
     lane_id = fields.Many2one('station.management.lane')
-    booking_date = fields.Datetime(default=lambda self: fields.Datetime.today())
+    booking_date = fields.Datetime(default=lambda self: fields.Datetime.today(), tracking=True)
     unit = fields.Float(required=True)
     currency_id = fields.Many2one('res.currency', related='station_id.currency_id')
     price = fields.Monetary(related='station_id.price_per_unit')
@@ -26,7 +26,7 @@ class StationBooking(models.Model):
         ('complete', 'Complete'),
         ('reschedule', 'Reschedule'),
 
-    ], default='draft')
+    ], default='draft', tracking=True)
 
     @api.model
     def create(self, values):
@@ -39,3 +39,41 @@ class StationBooking(models.Model):
         """Compute function for the Amenities"""
         for booking in self:
             booking.total = booking.price * booking.unit
+
+    def action_confirm(self):
+        for rec in self:
+            rec.state = 'confirm'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'cancel'
+
+    def action_paid(self):
+        for rec in self:
+            rec.state = 'paid'
+
+    def action_accepted(self):
+        for rec in self:
+            rec.state = 'accept'
+
+    def action_complete(self):
+        for rec in self:
+            rec.state = 'complete'
+
+    def action_reschedule(self):
+        for rec in self:
+            rec.state = 'reschedule'
+
+    def action_register_payment(self):
+        return {
+            'name': _('Register Payment'),
+            'res_model': 'account.payment.register',
+            'view_mode': 'form',
+            'views': [[False, 'form']],
+            'context': {
+                'active_model': 'account.move.line',
+                'active_ids': self.ids,
+            },
+            'target': 'new',
+            'type': 'ir.actions.act_window',
+        }
