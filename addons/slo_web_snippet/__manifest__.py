b646fd5a075c9b9c7a875b3c1bd382659db49ea7
SawLwinnOo <sawlwinoodev@gmail.com>
2024-06-27 13:41:41 +0630

    [add] snippet

---
+++ addons/slo_web_snippet/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'My Snippet',
+    'depends': ['website'],
+    'data':[
+        'views/my_snippet.xml'
+    ],
+    'author': 'SawLwinnOo',
+    'website': 'https://www.sawlwinoo.odoo.com/',
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3'
+}
